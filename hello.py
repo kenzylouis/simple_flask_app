fc167c1eeb4514bff8a04f4c29666d8c66839eb0
Kenzy Louis <klouis@cognitivescale.com>
2019-09-25 01:09:13 -0500

    corrected error - added routes - variable parts in URL

--- hello.py
+++ hello.py
@@ -13,3 +13,20 @@ def hello_world():
 @app.route('/hello/<name>')
 def hello(name):
     return "Hello, " + name
+
+## https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post  {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
