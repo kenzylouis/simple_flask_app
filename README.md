27bae8739584f9ba7a92a569af2fb60b02d2865c
Kenzy Louis <klouis@cognitivescale.com>
2019-09-25 00:49:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple Flask App with FromZeroToFlask
+
+## Run the Flask App
+
+```
+export FLASK_APP=hello
+flask run
+```
---
+++ pa-wsgi.py
@@ -0,0 +1,11 @@
+import sys
+python_anywhere_username = 'jorge3'
+path = '/home/' + python_anywhere_username + '/opt/simple_flask_app'
+if path not in sys.path:
+    sys.path.append(path)
+
+from hello import app
+app.debug = False
+
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(app, evalex=True)
